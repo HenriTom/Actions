d993e8311a30a2926612eeeafe1a6c4581dc18d4
Henri <Henri06@Mail.de>
2024-12-24 04:20:04 +0100

    Initial commit

---
+++ src/client/kotlin/de/henritom/actions/ActionsClient.kt
@@ -0,0 +1,9 @@
+package de.henritom.actions
+
+import net.fabricmc.api.ClientModInitializer
+
+object ActionsClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
